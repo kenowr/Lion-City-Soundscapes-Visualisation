8e67757fb394642bde46ad7c7df74d733cf884ec
nemopotatoes <jesgjx@gmail.com>
2023-02-14 11:35:14 +0800

    install django leaflet

--- visualisation/settings.py
+++ visualisation/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lioncitysoundscapes'
+    'lioncitysoundscapes',
+    'leaflet'
 ]
 
 MIDDLEWARE = [
