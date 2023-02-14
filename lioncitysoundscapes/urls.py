4b19d6ef27f43210d0165c25a16f9b4bd608fd87
nemopotatoes <jesgjx@gmail.com>
2023-02-15 02:31:35 +0800

    edited urls and index names

--- lioncitysoundscapes/urls.py
+++ lioncitysoundscapes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 # URLConf
 urlpatterns = [
-    path('about/', views.about_page),
-    path('map/', views.map_page)
+    path('about/', views.about_page, name='about'),
+    path('map/', views.map_page, name='map')
 ]
