20ce8a607ad4a03d986c2d1d8719b919dcf28d16
nemopotatoes <jesgjx@gmail.com>
2023-02-26 02:35:00 +0800

    added all markers to map

--- lioncitysoundscapes/static/map.js
+++ lioncitysoundscapes/static/map.js
@@ -1,46 +1,44 @@
+// Set up initial map center and zoom control
 var center = L.bounds([1.56073, 105.11475], [1.16, 103.502]).getCenter();
 var map = L.map('mapdiv', {
     zoomControl: false                  // remove top left zoom controls
 }).setView([1.350270, 103.829959],13);  // lat and long coordinates + initial zoom
 
-var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png', {
+// zoom buttons to bottom right
+L.control.zoom({ position: 'bottomright' }).addTo(map);
+
+// add tile layer
+var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Grey/{z}/{x}/{y}.png', {
     detectRetina: true,
     maxZoom: 20,
     minZoom: 11
 });
 
+// set max bounds
 map.setMaxBounds([[1.50073, 104.1147], [1.16, 103.602]]);
 
 basemap.addTo(map);
 
-// zoom buttons to bottom right
-L.control.zoom({ position: 'bottomright' }).addTo(map);
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } 
-}
+// get map data
+$.get('../../static/locations.csv', function(csvString) {
+    // var data = $.csv.toObjects(csvString);
 
-function showPosition(position) {           
-    marker = new L.Marker([position.coords.latitude, position.coords.longitude], {bounceOnAdd: false}).addTo(map);             
-    var popup = L.popup()
-    .setLatLng([position.coords.latitude, position.coords.longitude]) 
-    .setContent('You are here!')
-    .openOn(map);         
-}
+    // Use PapaParse to convert string to array of objects
+    var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
+    console.log(data);
 
+    // For each row in data, create a marker and add it to the map
+    // For each row, columns `Latitude`, `Longitude`, and `Title` are required
+    for (var i in data) {
+        var row = data[i];
 
-// openstreetmap
-// var map = L.map('map', {
-//     zoomControl: false                                              // remove top left zoom controls
-//   }).setView([1.350270, 103.829959],12.3);                          // lat and long coordinates
-  
-//   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     maxZoom: 19,
-//     attribution: 
-//     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//   }).addTo(map);
+        var marker = L.marker([
+            parseFloat(row.Latitude), 
+            parseFloat(row.Longitude)
+        ], { opacity: 1 }).bindPopup(row.Title);
+      
+      marker.addTo(map);
+    }
 
-//   //zoom buttons to bottom right
-//   L.control.zoom({ position: 'bottomright' }).addTo(map);
+});
