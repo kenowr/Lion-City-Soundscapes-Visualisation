84dc8f3070aff8a814baf9f1fbb63215b913acd7
nemopotatoes <jesgjx@gmail.com>
2023-02-26 16:38:24 +0800

    added custom map markers

--- lioncitysoundscapes/static/map.js
+++ lioncitysoundscapes/static/map.js
@@ -1,50 +1,57 @@
-window.onload = function() {
-    // Set up initial map center and zoom control
-    var center = L.bounds([1.56073, 105.11475], [1.16, 103.502]).getCenter();
-    var map = L.map('mapdiv', {
-        zoomControl: false                  // remove top left zoom controls
-    }).setView([1.350270, 103.829959],13);  // lat and long coordinates + initial zoom
+// Set up map
+var center = L.bounds([1.56073, 105.11475], [1.16, 103.502]).getCenter();
+var map = L.map('mapdiv', {
+    zoomControl: false                  // remove top left zoom controls
+}).setView([1.350270, 103.829959],13);  // lat and long coordinates + initial zoom
 
-    // zoom buttons to bottom right
-    L.control.zoom({ position: 'bottomright' }).addTo(map);
+// zoom buttons to bottom right
+L.control.zoom({ position: 'bottomright' }).addTo(map);
 
-    // add tile layer
-    var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Grey/{z}/{x}/{y}.png', {
-        detectRetina: true,
-        maxZoom: 20,
-        minZoom: 11
-    });
+// add tile layer
+var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Grey/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    maxZoom: 20,
+    minZoom: 11
+});
 
-    // set max bounds
-    map.setMaxBounds([[1.50073, 104.1147], [1.16, 103.602]]);
+// set max bounds
+map.setMaxBounds([[1.50073, 104.1147], [1.16, 103.602]]);
 
-    basemap.addTo(map);
+basemap.addTo(map);
 
-    // manually add markers
-    // var marker = L.marker([1.320611, 103.81609]).addTo(map);
-    // marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+// custom marker icons
+var LeafIcon = L.Icon.extend({
+    options: {
+        // shadowUrl: 'leaf-shadow.png',
+        iconSize:     [40, 40],
+        // shadowSize:   [50, 64],
+        // iconAnchor:   [22, 94],
+        // shadowAnchor: [4, 62],
+        popupAnchor:  [0, -25]
+    }
+});
+var chaoticRestless = new LeafIcon({iconUrl: '../../static/images/customIcons/pink-marker.png'}),
+    lifeExciting = new LeafIcon({iconUrl: '../../static/images/customIcons/orange-marker.png'}),
+    calmTranquil = new LeafIcon({iconUrl: '../../static/images/customIcons/green-marker.png'}),
+    boringLifeless = new LeafIcon({iconUrl: '../../static/images/customIcons/black-marker.png'});
 
-    // get map data
-    $.get('../../static/locations.csv', function(csvString) {
-        // var data = $.csv.toObjects(csvString);
+// manually add markers
+// var marker = L.marker([1.320611, 103.81609]).addTo(map);
+// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 
-        // Use PapaParse to convert string to array of objects
-        var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
-        console.log(data);
+// get map data
+$.get('../../static/locations.csv', function(csvString) {
+    // Use PapaParse to convert string to array of objects
+    var data = Papa.parse(csvString, {header: true, dynamicTyping: true}).data;
+    console.log(data);
 
-        // For each row in data, create a marker and add it to the map
-        // For each row, columns `Latitude`, `Longitude`, and `Title` are required
-        for (var i in data) {
-            var row = data[i];
-
-            var marker = L.marker([
-                parseFloat(row.Latitude), 
-                parseFloat(row.Longitude)
-            ], { opacity: 1 }).bindPopup(row.Title).openPopup();
-        
-          marker.addTo(map);
-        }
-    });
-};
-
-
+    // For each row in data, create a marker and add it to the map
+    for (var i in data) {
+        var row = data[i];
+        var marker = L.marker([
+            parseFloat(row.Latitude), 
+            parseFloat(row.Longitude)
+        ], { opacity: 1, icon: calmTranquil }).bindPopup(row.Title).openPopup();
+        marker.addTo(map);
+    }
+});
