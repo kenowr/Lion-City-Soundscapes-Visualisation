7ea1e473834eed8ca9ccdc0832649abec08f9553
nemopotatoes <jesgjx@gmail.com>
2023-02-26 22:58:16 +0800

    added layers control

--- lioncitysoundscapes/static/map.js
+++ lioncitysoundscapes/static/map.js
@@ -7,26 +7,11 @@ var map = L.map('mapdiv', {
 // zoom buttons to bottom right
 L.control.zoom({ position: 'bottomright' }).addTo(map);
 
-// add tile layer
-var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Grey/{z}/{x}/{y}.png', {
-    detectRetina: true,
-    maxZoom: 20,
-    minZoom: 11
-});
-
-// set max bounds
-map.setMaxBounds([[1.50073, 104.1147], [1.16, 103.602]]);
-
-basemap.addTo(map);
-
 // custom marker icons
 var LeafIcon = L.Icon.extend({
     options: {
-        // shadowUrl: 'leaf-shadow.png',
         iconSize:     [40, 40],
-        // shadowSize:   [50, 64],
         iconAnchor:   [20, 40],
-        // shadowAnchor: [4, 62],
         popupAnchor:  [0, -25]
     }
 });
@@ -34,6 +19,11 @@ var chaoticRestless = new LeafIcon({iconUrl: '../../static/images/customIcons/pi
     fulloflifeExciting = new LeafIcon({iconUrl: '../../static/images/customIcons/orange-marker.png'}),
     calmTranquil = new LeafIcon({iconUrl: '../../static/images/customIcons/green-marker.png'}),
     boringLifeless = new LeafIcon({iconUrl: '../../static/images/customIcons/black-marker.png'});
+
+const fulloflifeExciting_array = [];
+const chaoticRestless_array = [];
+const calmTranquil_array = [];
+const boringLifeless_array = [];
 
 // add markers
 $.get('../../static/locations.csv', function(csvString) {
@@ -46,19 +36,81 @@ $.get('../../static/locations.csv', function(csvString) {
         var row = data[i];
         // data clean soundscape type for custom marker
         if (row.Type == 'F&E') {
-            row.Type = fulloflifeExciting
+            row.Type = fulloflifeExciting;
+            // create marker
+            var marker = L.marker([
+                parseFloat(row.Latitude), 
+                parseFloat(row.Longitude)
+            ], { opacity: 1, icon: row.Type }).bindPopup(row.Title).openPopup();
+            fulloflifeExciting_layer.addLayer(marker);
+            fulloflifeExciting_array.push(marker);
+            console.log("marker pushed to F&E");
         } else if (row.Type == 'B&L') {
-            row.Type = boringLifeless
+            row.Type = boringLifeless;
+            // create marker
+            var marker = L.marker([
+                parseFloat(row.Latitude), 
+                parseFloat(row.Longitude)
+            ], { opacity: 1, icon: row.Type }).bindPopup(row.Title).openPopup();
+            boringLifeless_layer.addLayer(marker);
+            boringLifeless_array.push(marker);
+            console.log("marker pushed to B&L");
         } else if (row.Type == 'C&T') {
-            row.Type = calmTranquil
+            row.Type = calmTranquil;
+            // create marker
+            var marker = L.marker([
+                parseFloat(row.Latitude), 
+                parseFloat(row.Longitude)
+            ], { opacity: 1, icon: row.Type }).bindPopup(row.Title).openPopup();
+            calmTranquil_layer.addLayer(marker);
+            calmTranquil_array.push(marker);
+            console.log("marker pushed to C&T");
         } else if (row.Type == 'C&R') {
-            row.Type = chaoticRestless
-        } else { row.Type = NULL};
-        // create marker
-        var marker = L.marker([
-            parseFloat(row.Latitude), 
-            parseFloat(row.Longitude)
-        ], { opacity: 1, icon: row.Type }).bindPopup(row.Title).openPopup();
-        marker.addTo(map);
+            row.Type = chaoticRestless;
+            // create marker
+            var marker = L.marker([
+                parseFloat(row.Latitude), 
+                parseFloat(row.Longitude)
+            ], { opacity: 1, icon: row.Type }).bindPopup(row.Title).openPopup();
+            chaoticRestless_layer.addLayer(marker);
+            chaoticRestless_array.push(marker);
+            console.log("marker pushed to C&R");
+        } else ( null );
     }
 });
+
+// layer groups
+var fulloflifeExciting_layer = L.layerGroup(fulloflifeExciting_array).addTo(map);
+var chaoticRestless_layer = L.layerGroup(chaoticRestless_array).addTo(map);
+var calmTranquil_layer = L.layerGroup(calmTranquil_array).addTo(map);
+var boringLifeless_layer = L.layerGroup(boringLifeless_array).addTo(map);
+
+// add map tile layer
+var basemap = L.tileLayer('https://maps-{s}.onemap.sg/v3/Grey/{z}/{x}/{y}.png', {
+    detectRetina: true,
+    maxZoom: 20,
+    minZoom: 11,
+    layers: [
+        fulloflifeExciting_layer,
+        chaoticRestless_layer,
+        calmTranquil_layer,
+        boringLifeless_layer
+    ]
+}).addTo(map);
+
+// set max bounds of map
+map.setMaxBounds([[1.50073, 104.1147], [1.16, 103.602]]);
+
+// initialise overlay maps 
+var overlayMaps = {
+    "Full of Life and Exciting": fulloflifeExciting_layer,
+    "Chaotic and Restless": chaoticRestless_layer,
+    "Calm and Tranquil": calmTranquil_layer,
+    "Boring and Lifeless": boringLifeless_layer
+};
+
+// add overlay layers to map and pass an empty object for the base layers argument
+var layerControl = L.control.layers({}, overlayMaps, {
+    position: 'topleft',
+    className: 'typesLayerControl'
+}).addTo(map);
