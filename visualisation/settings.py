c5d6b94433f4c35af90b29dd4898e1595228e370
nemopotatoes <jesgjx@gmail.com>
2023-03-15 22:25:56 +0800

    removed allowed host ip address

--- visualisation/settings.py
+++ visualisation/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.116.52.177']
+ALLOWED_HOSTS = []
 
 
 # Application definition
